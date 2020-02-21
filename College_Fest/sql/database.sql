1f6823f06f80e4733ed285e5f2712f2cdd1ee805
Prudhvi-Nath <41383256+Prudhvi-Nath@users.noreply.github.com>
2020-02-21 17:45:20 +0530

    change in sql
    added sql table for login and sign up

--- College_Fest/sql/database.sql
+++ College_Fest/sql/database.sql
@@ -1,3 +1,6 @@
+set pagesize 100
+set linesize 100
+set num 10
 drop table fest_admin;
 drop table fest_event_Staff_create;
 drop table fest_event_org_create;
@@ -5,11 +8,11 @@ drop table fest_staff_ids;
   create table fest_admin(id varchar2(20),name varchar2(30),password varchar2(30),gender varchar2(10),age number(3),blood_grp varchar2(10),qualification varchar2(20),department varchar2(30),role varchar2(30),experience number(3), constraint fest_adm_pk primary key(id,name,department,role));
 create table fest_event_org_create(id varchar2(10),name varchar2(30),password varchar2(30),gender varchar2(20),mobileno number(10),dept varchar2(50),event varchar2(50), constraint fest_event_org_pk primary key(id,name,mobileno,event));
  create table fest_event_staff_create(sid varchar2(10),name varchar2(20),gender varchar2(10),mobileno number(10),event varchar2(30),orgid varchar2(10),department varchar2(70));
-alter table fest_event_Staff_create  add constraint p_k_staff primary key(sid,name,mobileno);
+alter table fest_event_staff_create  add constraint p_k_staff primary key(sid,name,mobileno);
 
 
 create table fest_staff_ids(id varchar2(10));
- alter table fest_Staff_ids add constraint p_k_staff_id primary key(id);
+ alter table fest_staff_ids add constraint p_k_staff_id primary key(id);
 insert into fest_admin values('admin','sujatha','festadmin','female',50,'AB-','MTECH','Computer Science','Vice Principal',10);
 insert into fest_staff_ids values('EVOS1');
 insert into fest_staff_ids values('EVOS2');
@@ -37,4 +40,4 @@ insert into fest_staff_ids values('EVOS23');
 insert into fest_staff_ids values('EVOS24');
 insert into fest_staff_ids values('EVOS25');
 commit;
-alter table fest_event_Staff_create  add constraint f_k_staff foreign key(orgid) references fest_event_org_create(id);
+alter table fest_event_staff_create  add constraint   f_k_staff foreign key(orgid)  references fest_event_org_create(id) on delete cascade;
---
+++ Login_and_SignUp_JEE/sql/database.sql
@@ -0,0 +1,5 @@
+set pagesize 100
+set linesize 100
+set num 10
+drop table emp10to1;
+create table emp10to1(EMAIL varchar2(10),FIRSTNAME varchar2(20),SECONDNAME varchar2(20),password varchar2(20),REENTERPASSWORD varchar2(20));
